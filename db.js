e53728bcf699b588f732b11c49a3406b89a64713
Brian <brian.hargrove@outlook.com>
2021-03-18 23:41:53 -0400

    next try

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+});
 
 // let Team = require('./models/teamModel');
 // let Stats = require('./models/tourneyStatModel');
